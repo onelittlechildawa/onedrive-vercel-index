ea01eff1995bfc79b694340eb27c35fbcb98a865
小小小朋友 <44793814+onelittlechildawa@users.noreply.github.com>
2023-01-10 13:36:01 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -33,7 +33,7 @@ module.exports = {
   // googleFontMono - the monospace font used in onedrive-vercel-index.
   googleFontMono: 'Fira Mono',
   // googleFontLinks -  an array of links for referencing the google font assets.
-  googleFontLinks: ['https://fonts.yecdn.com/css2?family=Fira+Mono&family=Inter:wght@400;500;700&display=swap'],
+  googleFontLinks: ['https://fonts.sourcegcdn.com/css2?family=Fira+Mono&family=Inter:wght@400;500;700'],
 
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
